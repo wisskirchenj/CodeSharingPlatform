2c10e839fd72a6c5d8c2735cf03c69f872cc33b9
wisskirchenj <wisskirchen.juergen@gmail.com>
2024-06-09 22:31:39 +0200

    renovate - esp. native image

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,18 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     java
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("org.graalvm.buildtools.native") version "0.10.1"
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "de.cofinpro"
-version = "0.6.6-SNAPSHOT"
+version = "0.7.0-SNAPSHOT"
 val dockerHubRepo = "wisskirchenj/"
+
+java.toolchain {
+    languageVersion.set(JavaLanguageVersion.of(22))
+}
 
 configurations {
     compileOnly {
@@ -55,9 +59,10 @@ tasks.named<AbstractAot>("processTestAot") {
 }
 
 tasks.named<BootBuildImage>("bootBuildImage") {
-//    buildpacks.set(listOf("paketobuildpacks/java:beta"))
+//    buildpacks.set(listOf("paketobuildpacks/java:latest"))
     buildpacks.set(listOf("paketobuildpacks/java-native-image:latest"))
     builder.set("paketobuildpacks/builder-jammy-buildpackless-tiny")
+    environment.put("BP_NATIVE_IMAGE_BUILD_ARGUMENTS", "-H:-AddAllFileSystemProviders")
     imageName.set(dockerHubRepo + rootProject.name + ":" + version)
     createdDate.set("now")
 }
